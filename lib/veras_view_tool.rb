e5ac835e2682c6bc1ec47864766cbdeffac82d7a
verasophie <vera@j2marketing.de>
2020-04-10 08:59:53 +0200

    Veras View Tool initial commit

---
+++ lib/veras_view_tool.rb
@@ -0,0 +1,7 @@
+require "veras_view_tool/version"
+require "veras_view_tool/renderer"
+
+module VerasViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/veras_view_tool/version.rb
@@ -0,0 +1,3 @@
+module VerasViewTool
+  VERSION = "0.1.0"
+end
